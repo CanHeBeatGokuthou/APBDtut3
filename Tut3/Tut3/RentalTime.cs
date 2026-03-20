22d0ed808ee7e6335332eec618e175ff135838d0
Kajetan Borkowski <kajetan.borkowski.com@gmail.com>
2026-03-20 14:53:03 +0100

    RentalTimeClass

---
+++ Tut3/Tut3/RentalTime.cs
@@ -0,0 +1,37 @@
+﻿namespace DefaultNamespace;
+
+public interface RentalTime
+{
+    public DateTime RentalDate { get; set;}
+    public DateTime ExpectedReturnDate { get; set;}
+    public DateTime? ReturnDate { get; set;}
+    
+    public void  SetReturnDate(DateTime date)
+    {
+        ReturnDate = date;
+    }
+
+    public void SetExpectedReturnDate(DateTime date)
+    {
+        ExpectedReturnDate = date;
+    }
+
+    public void setRentalDate(DateTime date)
+    {
+        RentalDate = date;
+    }
+
+    public TimeSpan? GetReturnTime()
+    {
+        if (ReturnDate != null)
+        {
+            return ReturnDate.Value-RentalDate;
+        }
+        return null;
+    }
+
+    public bool IsReturnDateValid()
+    {
+        return ReturnDate<=ExpectedReturnDate; 
+    }
+}
