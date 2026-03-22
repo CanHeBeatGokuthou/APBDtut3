300439d53df77449205e1e2643754c71ab1c7d7f
Kajetan Borkowski <kajetan.borkowski.com@gmail.com>
2026-03-22 11:16:19 +0100

    ApplyingRentalLogic

--- Tut3/Tut3/Employee.cs
+++ Tut3/Tut3/Employee.cs
@@ -3,7 +3,7 @@
 public class Employee:User
 {
     public override int MaxRentals => 5;
-    public string type { get; };
+    public string type { get; }
 
     public Employee(string name, string surname,string type) : base(name, surname)
     {
--- Tut3/Tut3/RentalService.cs
+++ Tut3/Tut3/RentalService.cs
@@ -2,7 +2,20 @@
 
 public class RentalService
 {
-    private List<User> users = new List<User>();
-    private List<Equipment> equipments = new List<Equipment>();
+    private readonly List<User> users = new();
+    private readonly List<Equipment> equipment = new();
+    private readonly List<SpecialRecord> rentals = new();
     
+    private const double DailyPenaltyRate = 10;
+
+    public void AddUser(User user) => users.Add(user);
+    public void AddEquipment(Equipment item) => equipment.Add(item);
+
+    public IEnumerable<Equipment> GetAllEquipment() => equipment;
+    public IEnumerable<Equipment> GetAvailableEquipment() => equipment.Where(e => e.available);
+
+    public void RentEqp(long userId, long equipmentId, DateTime date, int days)
+    {
+    }
+
 }
